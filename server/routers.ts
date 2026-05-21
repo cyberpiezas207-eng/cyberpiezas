5d84edf9ff9ba0781375b64bb91ee5623df218ad
cyberpiezas207-eng <cyberpiezas207@gmail.com>
2026-05-21 11:27:34 -0600

    Update routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -15,6 +15,7 @@ import { verduleriaRouter } from "./routers/verduleria";
 import { tarimaRouter } from "./routers/tarima";
 import { pagosRouter } from "./routers/pagos";
 import { taqueriaRouter } from "./routers/taqueria";
+import { staffRouter } from "./routers/staff";
 import { publicProcedure, router, protectedProcedure } from "./_core/trpc";
 import * as db from "./db";
 import { storagePut } from "./storage";
@@ -184,6 +185,7 @@ export const appRouter = router({
   tarima: tarimaRouter,
   pagos: pagosRouter,
   taqueria: taqueriaRouter,
+  staff: staffRouter,
   auth: router({
     login: publicProcedure
       .input(
